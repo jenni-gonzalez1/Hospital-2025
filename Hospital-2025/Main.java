7b8dd98d370fc88a1eb11433c74a98637cbc1dab
jenni-gonzalez1 <jpgonzalez@scl.edu.gt>
2025-05-19 09:33:20 -0600

    Add files via upload

---
+++ Hospital-2025/Main.java
@@ -0,0 +1,39 @@
+//crear anotaciones del paquete javax.swing 
+import java.awt.BorderLayout;  // Permite usar un layout que organiza en Norte, Sur, Este, Oeste y Centro
+import java.awt.Color; // Permite usar colores predefinidos
+import java.awt.Dimension;  // Se usa para definir dimensiones (ancho y alto)
+
+import javax.swing.*;  //cerar la ventana
+
+import Vistas.LoginPanel; // Importar un panel personalizado llamado LoginPanel desde el paquete Vistas
+
+
+//declaracion de la clase principal
+public class Main {
+
+    public static void Main(String[] args) {
+        
+        //crear una ventana con JFrame
+        JFrame miVentana = new JFrame();
+        miVentana.setTitle("Hospital");     //poner titulo 
+        miVentana.setSize(500,800);    //establecer el tamaño de la ventana (altura x ancho)
+        miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // Al cerrar la ventana, el programa termina completamente
+        miVentana.setLayout(new BorderLayout());   // Usar un BorderLayout para organizar los componentes (NORTE, SUR, ESTE, OESTE, CENTRO)
+        
+        //panel banner 
+        JPanel panelBanner = new JPanel();
+        panelBanner.setPreferredSize(new Dimension(500,800)); // Establecer tamaño preferido del panel (será considerado por el layout)
+        panelBanner.setSize(500, 300);// Establecer tamaño explícito (⚠️ puede ser ignorado por el layout manager)
+        panelBanner.setOpaque(true);     // Hacer que el fondo del panel sea visible (no transparente)
+        panelBanner.setBackground(Color.red);
+        miVentana.add(panelBanner,BorderLayout.WEST);  // Añadir el panel a la parte izquierda (WEST) de la ventana
+
+        //panel login
+        LoginPanel panelLogin = new LoginPanel();   // Crear una instancia del panel de login personalizado
+        miVentana.add(panelLogin.getPanel(), BorderLayout.CENTER);  // Añadir el panel de login en el centro de la ventana
+        
+        miVentana.setVisible(true);    //asegurarse que la etiqueta sigua siendo visible
+
+        }
+        
+    }
